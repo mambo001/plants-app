bd8cd371fd9521129e94a04742687d49911c4bdf
Reuben <reuben.gorda@gmail.com>
2025-11-26 22:48:56 +0800

    add routes to app entry point

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,56 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 import { PlantsProvider } from "./context";
 import { Home } from "./pages/home";
-import { AppBar } from "./app-bar";
+import { AppBar } from "./components/app-bar";
 import { ThemeProvider } from "@mui/material";
 import { appTheme } from "./theme";
+import { PlantDetail } from "./pages/plant-detail";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+});
+
+export function AppInternal() {
+  return (
+    <Routes>
+      <Route path="plants-app">
+        <Route
+          index
+          element={
+            <AppBar>
+              <Home />
+            </AppBar>
+          }
+        />
+        <Route path="plant">
+          <Route
+            path=":plantId"
+            element={
+              <AppBar>
+                <PlantDetail />
+              </AppBar>
+            }
+          />
+        </Route>
+      </Route>
+    </Routes>
+  );
+}
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <PlantsProvider>
         <ThemeProvider theme={appTheme}>
-          <AppBar>
-            <Home />
-          </AppBar>
+          <BrowserRouter>
+            <AppInternal />
+          </BrowserRouter>
         </ThemeProvider>
       </PlantsProvider>
     </QueryClientProvider>
