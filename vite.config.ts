7fd439bc9757e702f490fdd36e77991ab369f153
Reuben Mark Gorda <reuben.gorda@gmail.com>
2026-03-11 12:12:14 +0800

    rebuild gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   build: {
     outDir: "dist",
-    assetsDir: "",
+    assetsDir: "assets",
   },
-  base: "/plants-app/",
-});
+  base: mode === "development" ? "/" : "/plants-app/",
+}));
